919c20fcc63e92c3d9383b57c01ec3b47aedce40
Greenpools Ryan <greenpoo@oa-j1.msk.21-school.ru>
2023-11-09 15:05:05 +0300

    [task-1/feature]: strncat

--- string/string.c
+++ string/string.c
@@ -11,3 +11,13 @@ size_t strlen(const char *str) {
     return result;
 }
 
+char *strncat(char *dest, const char *src, size_t n) {
+    size_t destLength = strlen((const char*)dest);
+    char* tmp = (char*) src;
+
+    for (int i = 0; tmp != '\0' || i < n; i++, tmp++, destLength++)
+        dest[destLength] = tmp;
+    dest[destLength] = '\0';
+
+    return dest;
+}
