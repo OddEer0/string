416f6026460c6083f0dbe7d641b27f95d7e1d29d
Iv Porter <ivp@MacBook-Air-Iv.local>
2023-11-13 15:19:55 +0300

    add new funk in H

--- s21_string.h
+++ s21_string.h
@@ -17,3 +17,10 @@ char *s21_strpbrk(const char *str1, const char *str2);
 char *s21_strrchr(const char *str, int c);
 char *s21_strstr(const char *haystack, const char *needle);
 char *s21_strtok(char *str, const char *delim);
+
+// to do next* For task 5
+
+void *to_upper(const char *str) // Returns a copy of string (str) converted to uppercase. In case of any error, return NULL
+void *to_lower(const char *str) // Returns a copy of string (str) converted to lowercase. In case of any error, return NULL
+void *insert(const char *src, const char *str, size_t start_index) // Returns a new string in which a specified string (str) is inserted at a specified index position (start_index) in the given string (src). In case of any error, return NULL
+void *trim(const char *src, const char *trim_chars) // Returns a new string in which all leading and trailing occurrences of a set of specified characters (trim_chars) from the given string (src) are removed. In case of any error, return NULL
