242f5bc3c64112cc128f744a7959f6023aeec15d
Greenpools Ryan <greenpoo@at-k4.msk.21-school.ru>
2023-11-24 16:27:21 +0300

    [task-11/feature]: intToStr realiztion

--- sprintf/main.c
+++ sprintf/main.c
@@ -7,29 +7,74 @@
 
 #define BUFF_SIZE 500
 
+void swapp(char *x, char *y) {
+    char t = *x; *x = *y; *y = t;
+}
+
+long long llabss(long long int x) {
+    return x < 0 ? -x : x;
+}
+ 
+char* reversee(char *buffer, int i, int j) {
+    while (i < j) {
+        swapp(&buffer[i++], &buffer[j--]);
+    }
+ 
+    return buffer;
+}
+
+int intToStrr(long long int x, char str[], int afterpoint) { 
+    int i = 0;
+    int isNegative = x < 0;
+
+    long long $value = llabss(x);
+    long long int j = $value;
+    int isRound = 0;
+
+    for (long long int j = $value; j; j /= 10, i++) {
+        if (!i) {
+            int tmp = j % 10;
+            if (tmp >= 5)
+                isRound = 1;
+        } else {
+            str[i - 1] = (j % 10) + '0';
+        }
+    }
+
+    printf("round: %d\n", isRound);
+
+    for (int j = 0; j < i - 1 && isRound; j++) {
+        str[j] += 1;
+        if (str[j] > '9') {
+            str[j] = '0';
+        } else {
+            isRound = 0;
+        }
+    }
+ 
+    for (; i < afterpoint; i++) 
+        str[i] = '0';
+
+    if (isNegative)
+        str[i++] = '-'; 
+ 
+    reversee(str, 0, i - 2);
+    str[i] = '\0';
+    return i; 
+} 
+
 int main() {
-    setlocale(LC_ALL, "");
-    // char format[] = "print: %% %#% %-020.10% %.10l% %10h% % #L% %d";
-    char format[] = "print: %020p lol";
+    long double value = 3.0;
+    long long ipart = (long long)value;
+    long double fpart = value - ipart;
+    for (int i = 0; i < 4; i++) {
+        fpart *= 10.0;
+    }
+    char tmp[100] = "";
 
-    char buff[BUFF_SIZE];
-    char buffOrig[BUFF_SIZE];
-    int result = sprintf(buffOrig, format, buff);
-    int resultC = s21_sprintf(buff, format, buff);
+    intToStrr((long long)(fpart * 10.0), tmp, 1);
 
-    printf("original: %s*\n", buffOrig);
-    printf("custom  : %s*\n", buff);
-    printf("original return: %d\n", result);
-    printf("custom return  : %d\n", resultC);
-
-    // setlocale(LC_ALL, "C.UTF-8"); // this also works
-
-    wchar_t hello_eng[] = L"Hello World!";
-    wchar_t hello_china[] = L"世界, 你好!";
-    wchar_t *hello_japan = L"こんにちは日本!";
-    printf("%ls\n", hello_eng);
-    printf("%ls\n", hello_china);
-    printf("%ls\n", hello_japan);
+    printf("%s\n", tmp);
 
     return 0;
 }
