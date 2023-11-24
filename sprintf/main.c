2dd4dc6969675a403dd30d7474db3a7fb0e2a936
Greenpools Ryan <greenpoo@mi-l3.msk.21-school.ru>
2023-11-24 20:17:32 +0300

    [task-11/feature]: save

--- sprintf/main.c
+++ sprintf/main.c
@@ -3,6 +3,7 @@
 #include <locale.h>
 #include <stdlib.h>
 #include <wchar.h>
+#include <string.h>
 
 
 #define BUFF_SIZE 500
@@ -23,7 +24,7 @@ char* reversee(char *buffer, int i, int j) {
     return buffer;
 }
 
-int intToStrr(long long int x, char str[], int afterpoint) { 
+int mantisToStr(long long int x, char str[], int afterpoint, int* $isRound) { 
     int i = 0;
     int isNegative = x < 0;
 
@@ -41,7 +42,6 @@ int intToStrr(long long int x, char str[], int afterpoint) {
         }
     }
 
-    printf("round: %d\n", isRound);
 
     for (int j = 0; j < i - 1 && isRound; j++) {
         str[j] += 1;
@@ -51,6 +51,8 @@ int intToStrr(long long int x, char str[], int afterpoint) {
             isRound = 0;
         }
     }
+    
+    *$isRound = isRound;
  
     for (; i < afterpoint; i++) 
         str[i] = '0';
@@ -61,20 +63,97 @@ int intToStrr(long long int x, char str[], int afterpoint) {
     reversee(str, 0, i - 2);
     str[i] = '\0';
     return i; 
+}
+
+int intToStrr(long long int x, char str[], int afterpoint) { 
+    int i = 0;
+    int isNegative = x < 0;
+
+    long long $value = llabss(x);
+
+    for (long long int j = $value; j; j /= 10, i++)
+        str[i] = (j % 10) + '0';
+ 
+    for (; i < afterpoint; i++) 
+        str[i] = '0';
+
+    if (isNegative)
+        str[i++] = '-'; 
+ 
+    reversee(str, 0, i - 1);
+    str[i] = '\0';
+    return i; 
 } 
 
-int main() {
-    long double value = 3.0;
-    long long ipart = (long long)value;
-    long double fpart = value - ipart;
-    for (int i = 0; i < 4; i++) {
-        fpart *= 10.0;
+void s21_fftoa(long double n, char* res, int afterpoint) {
+    long long ipart = (long long)n;
+    int aftercopy = afterpoint;
+    long double fpart = n - (long double)ipart;
+ 
+    // int length = intToStr(ipart, res, 0);
+    char temp[afterpoint + 1];
+    int isRound = 0, tempEnd = 0;
+ 
+    if (afterpoint != 0) { 
+
+        long double tmp = 0.0;
+        int tmpAfterpoint = afterpoint;
+        int length = 0;
+        for (int j = 0; j < tmpAfterpoint; j++) {
+            if (tmp == 0.0) tmp = 10.0;
+            else tmp *= 10.0;
+
+            if (tmp >= 1000000.0) {
+                fpart *= tmp;
+                tempEnd = mantisToStr((long long)(fpart * 10), temp + length, 6, &isRound);
+                length += 6;
+                fpart -= (int)fpart;
+                afterpoint -= 6;
+                tmp = 0.0;
+            }
+        }
+
+        if (tmp != 0.0) {
+            tmp = 10.0;
+            for (int i = 0; i < afterpoint - 1; i++)
+                tmp *= 10.0;
+            fpart *= tmp;
+
+            tempEnd = mantisToStr((long long)(fpart * 10), temp + length, afterpoint, &isRound); 
+        }
+
+        if (isRound) {
+            for (int end = aftercopy - tempEnd; end >= 0 && isRound; end--) {
+                temp[end] += 1;
+                if (temp[end] > '9') {
+                    temp[end] = '0';
+                } else {
+                    isRound = 0;
+                }
+                printf("%c %d\n", temp[end], end);
+            }
+        }
+
+        if (isRound)
+            ipart++;
     }
+    int resLength = intToStrr(ipart, res, 0);
+    if (aftercopy != 0) {
+        res[resLength] = '.';
+        res[resLength + 1] = '\0';
+        resLength++;
+
+        strncat(res, temp, aftercopy);
+    }
+        
+}
+
+int main() {
+    long double value = 3.9999;
     char tmp[100] = "";
+    s21_fftoa(value, tmp, 7);
 
-    intToStrr((long long)(fpart * 10.0), tmp, 1);
-
-    printf("%s\n", tmp);
+    printf("%s, %.7f\n", tmp, 3.9999);
 
     return 0;
 }
